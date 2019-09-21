c8c3d864ce4b2bb302bf82345330d560b16f8295
Oleh Kravchenko <oleg@kaa.org.ua>
2019-09-21 15:46:47 +0300

    Improve nice.sh
    - use nice && ionice && chrt
    - allow run commands
    
    Signed-off-by: Oleh Kravchenko <oleg@kaa.org.ua>

--- custom/feature/nice.sh
+++ custom/feature/nice.sh
@@ -2,10 +2,37 @@
 
 set -e
 
-if [ -z $1 ]; then
-	>&2 echo "Usage: $0 PID"
-	exit 1
-fi
+NAME=${0##*/}
 
-ionice -c 3 -p $1
-chrt --verbose --idle -p 0 $1
+function show_usage() {
+	cat << EOF
+Set minimum execution priority:
+
+Usage: ${NAME} -p PID
+Usage: ${NAME} COMMAND [ARG...]
+EOF
+}
+
+while getopts "p:h" opt; do
+	case $opt in
+	p)
+		PID=${OPTARG};;
+	h)
+		show_usage
+		exit 0;;
+	?)
+		show_usage
+		exit 1;;
+	esac
+done
+
+PID=${PID:-$$}
+
+chrt --idle -p 0 "${PID}"
+ionice -c 3 -p "${PID}"
+renice -n 19 -p "${PID}" > /dev/null
+
+shift $(($OPTIND - 1))
+((${#} == 0)) && exit 0
+
+exec ${@}
