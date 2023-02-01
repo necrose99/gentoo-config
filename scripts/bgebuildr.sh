0860ce63dca53b25d7d604296ef79f3c0951176f
Michael Lawnce <mike@michaellawrenceit.com>
2023-02-01 13:27:34 -0600

    Update bgebuildr.sh

--- scripts/bgebuildr.sh
+++ scripts/bgebuildr.sh
@@ -2,5 +2,7 @@
 #./bgebuildr to run in background
 ## ssh and fire up if disconected it will keep going. 
 ## else  also cronjob.. builds world binaries when posible but dose not yet merge them.. 
+# useful on rpi4 or remote arm64 / etc hosts for deoing emerge jobs and closing out ssh /resume latter
+# ie emerge @universe ..... only millions of year......s... seemingly start the build n drop 
 
 nohup ebuildr > /dev/null 2>&1 &
